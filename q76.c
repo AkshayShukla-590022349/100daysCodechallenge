c05f0b095639b3aae1c50f352d579e2e04c75677
AkshayShukla-590022349 <Akshay.22349@stu.upes.ac.in>
2025-11-29 01:45:46 +0530

    Create q76.c

---
+++ q76.c
@@ -0,0 +1,33 @@
+/*
+Check if a matrix is symmetric.
+*/
+
+#include <stdio.h>
+int main(){
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int matrix[r][c];
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int is_symmetric = 1;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            if(matrix[i][j] != matrix[j][i]){
+                is_symmetric = 0;
+                break;
+            }
+        }
+        if(!is_symmetric){
+            break;
+        }
+    }
+    if(is_symmetric){
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+    return 0;
+}
