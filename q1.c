8b846afbd627b6045849c9d67736c0a8ccaa5100
AkshayShukla-590022349 <Akshay.22349@stu.upes.ac.in>
2025-11-28 23:37:31 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,8 @@
+// q1.Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main(){
+    int a, b; 
+    scanf("%d%d", &a, &b); 
+    printf("sum = %d", a + b);
+    return 0;
+}
