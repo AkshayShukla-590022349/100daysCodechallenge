b3ff41ea7a882bb3d77681bf373a623783d660e3
AkshayShukla-590022349 <Akshay.22349@stu.upes.ac.in>
2025-11-29 01:59:34 +0530

    Create q97.c

---
+++ q97.c
@@ -0,0 +1,17 @@
+/*
+Print the initials of a name.
+*/
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char name[100];
+    fgets(name, sizeof(name), stdin);
+    int len = strlen(name);
+    for (int i = 0; i < len; i++) {
+        if (i == 0 || (name[i - 1] == ' ' && name[i] != ' ')) {
+            printf("%c.", name[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
