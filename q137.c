7a86db325e23dc515a158eae2db98696161ae4aa
AkshayShukla-590022349 <Akshay.22349@stu.upes.ac.in>
2025-11-29 02:44:21 +0530

    Create q137.c

---
+++ q137.c
@@ -0,0 +1,28 @@
+//Create an enum for user roles (ADMIN, USER, GUEST) and display messages based on role.
+
+#include <stdio.h>
+typedef enum {
+    ADMIN,
+    USER,
+    GUEST
+} UserRole;
+void displayMessage(UserRole role) {
+    switch(role) {
+        case ADMIN:
+            printf("Welcome Admin!\n");
+            break;
+        case USER:
+            printf("Welcome User!\n");
+            break;
+        case GUEST:
+            printf("Welcome Guest!\n");
+            break;
+        default:
+            printf("Unknown Role!\n");
+    }
+}
+int main() {
+    UserRole role = GUEST; 
+    displayMessage(role);
+    return 0;
+}
