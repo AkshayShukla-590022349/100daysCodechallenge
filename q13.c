2554f9dca80869f58fa545be5c282aa2916b9b38
AkshayShukla-590022349 <Akshay.22349@stu.upes.ac.in>
2025-11-29 00:58:06 +0530

    Create q13.c

---
+++ q13.c
@@ -0,0 +1,11 @@
+ // Write a program to input a year and check whether it is a leap year or not using conditional statements.#include <stdio.h>
+int main() {
+    int year;
+    scanf("%d", &year);
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("Leap year.\n", year);
+    } else {
+        printf("Not a leap year.\n", year);
+    }
+    return 0;
+}
